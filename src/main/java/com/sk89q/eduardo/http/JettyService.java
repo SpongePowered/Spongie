151b5fc123b048c2b4b105b0aeb18593587a593c
sk89q <the.sk89q@gmail.com>
2014-09-16 03:44:25 -0700

    Remove unneeded service interfaces.

--- src/main/java/com/sk89q/eduardo/http/JettyService.java
+++ src/main/java/com/sk89q/eduardo/http/JettyService.java
@@ -19,10 +19,60 @@
 
 package com.sk89q.eduardo.http;
 
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import com.google.inject.Inject;
+import com.sk89q.eduardo.event.StartupEvent;
+import com.typesafe.config.Config;
+import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public interface JettyService {
+import java.util.ArrayList;
+import java.util.List;
 
-    void registerHandler(Handler handler);
+public class JettyService {
+
+    private static final Logger log = LoggerFactory.getLogger(JettyService.class);
+
+    private final Config config;
+    private final List<Handler> handlers = new ArrayList<Handler>();
+
+    @Inject
+    public JettyService(Config config, EventBus eventBus) {
+        this.config = config;
+        eventBus.register(this);
+    }
+
+    @Subscribe
+    public void onStartup(StartupEvent event) {
+        Server server = new Server();
+
+        SelectChannelConnector connector = new SelectChannelConnector();
+        connector.setHost(config.getString("http.bind_address"));
+        connector.setPort(config.getInt("http.port"));
+        server.setConnectors(new Connector[] { connector });
+
+        ContextHandlerCollection contexts = new ContextHandlerCollection();
+        Handler[] handlersArr = new Handler[handlers.size()];
+        handlers.toArray(handlersArr);
+        contexts.setHandlers(handlersArr);
+        server.setHandler(contexts);
+
+        try {
+            server.start();
+        } catch (Exception e) {
+            log.info("Failed to start server", e);
+        }
+    }
+
+    public void registerHandler(Handler handler) {
+        handlers.add(handler);
+    }
+
 
 }
--- src/main/java/com/sk89q/eduardo/irc/PircBotXService.java
+++ src/main/java/com/sk89q/eduardo/irc/PircBotXService.java
@@ -19,6 +19,97 @@
 
 package com.sk89q.eduardo.irc;
 
-public interface PircBotXService {
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.sk89q.eduardo.event.BroadcastEvent;
+import com.sk89q.eduardo.event.StartupEvent;
+import com.typesafe.config.Config;
+import org.pircbotx.Configuration;
+import org.pircbotx.Configuration.Builder;
+import org.pircbotx.MultiBotManager;
+import org.pircbotx.PircBotX;
+import org.pircbotx.UtilSSLSocketFactory;
+import org.pircbotx.hooks.Event;
+import org.pircbotx.hooks.Listener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Singleton
+public class PircBotXService implements Listener<IrcBot> {
+
+    private static final Logger log = LoggerFactory.getLogger(PircBotXService.class);
+
+    private final Map<String, Builder<IrcBot>> builders = new HashMap<>();
+    private final MultiBotManager<IrcBot> manager = new MultiBotManager<>();
+    @Inject private EventBus eventBus;
+
+    @Inject
+    public PircBotXService(Config config, EventBus eventBus) {
+        eventBus.register(this);
+
+        Config irc = config.getConfig("irc");
+
+        Builder<IrcBot> base = new Configuration.Builder<IrcBot>()
+                .setVersion(irc.getString("version"))
+                .setAutoSplitMessage(true)
+                .setIdentServerEnabled(false)
+                .setAutoNickChange(true);
+
+        for (Config server : irc.getConfigList("servers")) {
+            server = server.withFallback(irc.getConfig("default-server"));
+
+            Builder<IrcBot> builder = new Builder<>(base)
+                    .setServer(server.getString("host"), server.getInt("port"), server.getString("password"));
+
+            builder
+                    .setName(server.getString("name"))
+                    .setLogin(server.getString("name"))
+                    .setFinger(server.getString("name"))
+                    .setRealName(server.getString("name"));
+
+            if (server.getBoolean("ssl")) {
+                builder.setSocketFactory(new UtilSSLSocketFactory().trustAllCertificates());
+            }
+
+            for (String channel : server.getStringList("auto-join")) {
+                log.info("Auto-joining channel {}", channel);
+                builder.addAutoJoinChannel(channel);
+            }
+
+            builders.put(server.getString("id"), builder);
+        }
+    }
+
+    @Subscribe
+    public void onStartup(StartupEvent event) {
+        for (Map.Entry<String, Builder<IrcBot>> entry : builders.entrySet()) {
+            Builder<IrcBot> builder = entry.getValue();
+            builder.addListener(this);
+            manager.addBot(new IrcBot(builder.buildConfiguration(), entry.getKey()));
+        }
+
+        Thread thread = new Thread(manager::start, "PircBotX");
+        thread.start();
+    }
+
+    @Subscribe
+    public void onBroadcast(BroadcastEvent event) {
+        String target = event.getTarget();
+        if (target.startsWith("#")) {
+            for (PircBotX bot : manager.getBots()) {
+                bot.sendIRC().message(target, event.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void onEvent(Event<IrcBot> event) throws Exception {
+        eventBus.post(event);
+    }
 
 }
